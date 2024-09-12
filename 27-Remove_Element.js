49342705f687e35301bd63be7c3d7c3433686ed0
Jake Altabef <ja63604@gmail.com>
2024-09-12 15:29:55 -0400

    Working on Easy problem 27. initial solution.

---
+++ 27-Remove_Element.js
@@ -0,0 +1,21 @@
+/**
+ * https://leetcode.com/problems/remove-element/
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+
+var removeElement = function(nums, val) {
+    if (nums.length == 0) {return 0;}
+
+    var k = 0, i;
+
+    for (i = 0; i < nums.length; i++) {
+        if (nums[i] != val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+}
