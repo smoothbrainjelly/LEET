e6abfbcf68a256bfba3dac8967db5ef648aa0005
Jake Altabef <ja63604@gmail.com>
2024-09-16 16:04:10 -0400

    added a section for neet150, completed one problem on stacks, dont love solution but it works

---
+++ NeetCode-150/Validate_Parentheses.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * https://neetcode.io/problems/validate-parentheses
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        if ( ['}',')',']'].includes(s[0]) ) { return false; }
+
+        var stack = [];
+        for (let i = 0; i < s.length; i++) {
+            switch (s[i]) {
+                case '{':
+                case '[':
+                case '(':
+                    stack.push(s[i]);
+                    break;
+                case '}':
+                    if (stack.pop() != '{') { return false; }
+                    break;
+                case ']':
+                    if (stack.pop() != '[') { return false; }
+                    break;
+                case ')':
+                    if (stack.pop() != '(') { return false; }
+                    break;
+                default:
+            }
+        }
+
+        return stack.length == 0;
+    }
+}
